7c05d11b5192dc87495d4b2749739b308669dea0
fan123199 <fan123199@gmail.com>
2015-12-26 17:58:39 +0800

    fixed bug for isbn10, change jquery to newest, fixed loged in bugs.

--- inject.js
+++ inject.js
@@ -12,77 +12,80 @@ function conv13to10(str) {
         result += c;
     }
     checkDigit = (11 - (checkDigit % 11)) % 11;
-    result += checkDigit == 10 ? 'X' : (checkDigit + "");
+    result += checkDigit == 10 ? "X" : (checkDigit + "");
 
     return ( result );
 }
 
-//获取图书信息
-function getInfo (isbn) {
-    var preurl = 'http://opac.lib.szu.edu.cn/opac/searchresult.aspx?isbn_f=' + isbn;
 
-    var jqxhr = $.ajax(preurl)
+//获取图书信息
+function getInfo (isbn_info) {
+    var preUrl = "http://opac.lib.szu.edu.cn/opac/searchresult.aspx?isbn_f=" + isbn_info;
+
+    var jqxhr = $.ajax({
+    	url:preUrl,
+    	cache: false})
 		.done(function(msg) {
-	        if (msg.indexOf('searchnotfound') != -1) {
-	            if(isbn.length == 13) {
-	            	getInfo(conv13to10(isbn));//检查isbn10
+	        if (msg.indexOf("searchnotfound") != -1) {
+	            if (isbn_info.length == 13) {
+	            	getInfo(conv13to10(isbn_info));
 	            }
 	            else {
-	            	$('#szulib').append("没有找到！");
+	            	$("#szulib").append("没有找到！");
 	            }                   
 	        }
 	        else {
 	            //取得信息后准备第二次连接
-	            $('#isex').html('有！');
-	            $('#isex').after('<br><h2>在这里</h2>');
-	            var findurl = msg.replace(/( |　|\r\n|\n)/ig, '')
-	            	.split('<spanclass="title"><ahref="')[1].split('"target="_blank"')[0];
+	            $("#isex").html("有！");
+	            $("#isex").after("<br><h2>在这里</h2>");
+	            var findurl = msg.replace(/( |　|\r\n|\n)/ig, "")
+	            	.split("<spanclass=\"title\"><ahref=\"")[1].split("\"target=\"_blank\"")[0];
 	            var info_url = "http://opac.lib.szu.edu.cn/opac/" + findurl;
 	            showinfo(info_url);
 	           
 			}
 		})
 		.fail(function() {
-	        $('#szulib').append("连接图书馆出错！");
+	        $("#szulib").append("连接图书馆出错！");
 	    });
 }
-function showinfo(url){
+function showinfo(url) {
 	 $.ajax({
 		url:url,
-		type: 'GET'})
+		cache: false})
 		.done(function (detail) {
-	        var detail_table = detail.split('<h5 class="tbc">藏书情况</h5>')[1].split('<input')[0];
-	        $('#szulib').append(detail_table + '<br><h2 class="bs" id="mdt"><a href="' 
-	        	+ url + '" target="_blank">点击查看详细</a></h2>');
+	        var detail_table = detail.split("<h5 class=\"tbc\">藏书情况</h5>")[1].split("<input")[0];
+	        $("#szulib").append(detail_table + "<br><h2 class=\"bs\" id=\"mdt\"><a href=\"" 
+	        	+ url + "\" target=\"_blank\">点击查看详细</a></h2>");
 
 
-	        $('.tbhead tr td:eq(2)').hide();
-	        $('.tbhead tr td:eq(3)').hide();
-	        $('.tbhead tr td:eq(4)').hide();
-	        $('.tbhead tr td:eq(6)').hide();
+	        $(".tbhead tr td:eq(2)").hide();
+	        $(".tbhead tr td:eq(3)").hide();
+	        $(".tbhead tr td:eq(4)").hide();
+	        $(".tbhead tr td:eq(6)").hide();
 
-	        for (var i = 1; i <= $('#szulib div .tb tbody tr').length ; i++) {
-	            $('.tb tbody tr td').eq(7 * i - 5).hide();
-	            $('.tb tbody tr td').eq(7 * i - 4).hide();
-	            $('.tb tbody tr td').eq(7 * i - 3).hide();
-	            $('.tb tbody tr td').eq(7 * i - 1).hide();
+	        for (var i = 1; i <= $("#szulib div .tb tbody tr").length ; i++) {
+	            $(".tb tbody tr td").eq(7 * i - 5).hide();
+	            $(".tb tbody tr td").eq(7 * i - 4).hide();
+	            $(".tb tbody tr td").eq(7 * i - 3).hide();
+	            $(".tb tbody tr td").eq(7 * i - 1).hide();
 	        }
 
-	        $('#szulib tbody').find('a').contents().unwrap();
+	        $("#szulib tbody").find("a").contents().unwrap();
 		})
 		.fail(function () {
-		    $('#szulib').append('获取信息失败!');
+		    $("#szulib").append("获取信息失败!");
 		});
 }
 
 function init () {
-    //未处理在图书馆登录后的情况
+
     //添加与豆瓣风格一样的div
-    $('#buyinfo').before('<div class="gray_ad" id="szulib"></div>');
+    $("#buyinfo").before("<div class=\"gray_ad\" id=\"szulib\"></div>");
     //首先快速载入问题（无需连接到图书馆）
-    $('#szulib').append('<h2>深大图书馆有没有?</h2><div class="bs" id="isex"></div>');
+    $("#szulib").append("<h2>深大图书馆有没有?</h2><div class=\"bs\" id=\"isex\"></div>");
     //获取isbn，text()获取html的文本， split分割
-    var isbn = $('#info').text().split('ISBN: ')[1];
+    var isbn = $("#info").text().split("ISBN: ")[1].trim();
     getInfo(isbn);
 }
 
